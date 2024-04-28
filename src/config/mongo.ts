38e3449f75b3ddd0b1549b74bf7a4df0d9f550c8
Mariana <marianagg0412@gmail.com>
2024-04-27 19:22:05 -0500

    fix leak

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import { connect } from "mongoose";
 
 async function dbConnect() :Promise<void>{
-    const DB_URI = process.env.DB_URI;
+    const DB_URI = process.env.DB_URI || 'failed to get db uri';
     console.log(DB_URI);
     await connect(DB_URI);
 }
